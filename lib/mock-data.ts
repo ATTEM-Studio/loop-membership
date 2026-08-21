a9eec2a18b4629c9009ad5f5e7ce9883c9996d73
ATTEM <brandoit.choi@gmail.com>
2026-08-21 15:34:00 +0900

    Add lib/mock-data.ts

---
+++ lib/mock-data.ts
@@ -0,0 +1,6 @@
+import {Customer} from './domain'
+export const initialCustomers:Customer[]=[
+ {id:'1',phone:'01012345678',source:'인스타',visits:8,points:8,lastVisit:'2026-08-21'},
+ {id:'2',phone:'01098765432',source:'네이버',visits:5,points:5,lastVisit:'2026-08-20'},
+ {id:'3',phone:'01045671234',source:'지나가다가',visits:3,points:3,lastVisit:'2026-08-19'},
+ {id:'4',phone:'01088332211',source:'당근',visits:2,points:2,lastVisit:'2026-08-18'}]
